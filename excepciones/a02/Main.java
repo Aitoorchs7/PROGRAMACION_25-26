9e38f256584f9db6195b931b169000ba9bc3b217
ACHS7 <chicanosellesaitor@gmail.com>
2026-04-14 09:17:21 +0200

    a02 terminada

--- excepciones/a02/Main.java
+++ excepciones/a02/Main.java
@@ -3,17 +3,38 @@ package excepciones.a02;
 import java.io.*;
 
 public class Main {
+        static final String RUTA_ARCHIVO = "C:\\Users\\ALUMNO\\Desktop\\Proyectos_java\\bucles\\a01\\A01.java";
     public static void main(String [] args){
-        FileReader a = null;
+        BufferedReader bufferedReader = null;
+        String linea = null;
 
         try{
-            a = new FileReader("C:\\Users\\ALUMNO\\Desktop\\Proyectos_java\\bucles\\a01\\A01.java");
-        }catch(FileNotFoundException | IOException e){
-            System.out.println(e.getMessage());
+            FileReader fileReader = new FileReader(RUTA_ARCHIVO);
+            bufferedReader = new BufferedReader(fileReader);
+            
+            while ((linea = bufferedReader.readLine()) != null) {
+                System.out.println(linea);
+            }
+            
+            bufferedReader.close();
+            
+        }catch(FileNotFoundException e){
+            System.out.println("Archivo no encontrado");
+        }catch(IOException e){
+            System.out.println("Error al leer el archivo: " + e  + "( .getMessage() tambien se puede usar)" );
         }finally{
-            System.out.println("Fin del programa");
+            if(linea != null){
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    System.out.println("Error al cerrar el archivo: " + e.getMessage());
+                }finally{
+                System.out.println("Archivo leido correctamente");
+                }
+            }
 
-                   
+            System.out.println("\n Fin del programa");
+            
         }
         
         
